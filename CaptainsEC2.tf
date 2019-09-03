2af3c517401c132fc173e87d110d92ee8aa4b8a7
Prakash Jha <prakashchandrajha@outlook.com>
2019-09-03 11:29:54 +0530

    Added S3 bucket dependency on Captains EC2

--- CaptainsEC2.tf
+++ CaptainsEC2.tf
@@ -3,8 +3,25 @@ provider "aws" {
   region  = "us-east-1"
 }
 
-resource "aws_instance" "CaptainsEC2_tf" {
-  ami           = "ami-2757f631"
-  instance_type = "t2.micro"
+resource "aws_s3_bucket" "captains_s3_bucket" {
+  bucket = "captains-s3-bucket"
+  acl = "private"
 }
 
+resource "aws_instance" "CaptainsEC2_tf" {
+  ami           = "ami-b374d5a5"
+  instance_type = "t2.micro"
+
+  # Adding dependency on S3 bucket to be created first
+  depends_on = [aws_s3_bucket.captains_s3_bucket]
+
+  # Specifying a provisoner (local-exec)
+  provisoner "local-exec" {
+    command = "echo ${aws_instance.CaptainsEC2_tf.id} > ip_address.txt"
+  }
+}
+
+resource "aws_eip" "ip" {
+  vpc = true
+  instance = aws_instance.CaptainsEC2_tf.id
+}
--- Connections.tf
+++
@@ -1,6 +0,0 @@
-provider "aws" {
-  version = "~>2.0"
-  region  = "ap-south-1"
-  profile = "ViratKohli"
-}
-
